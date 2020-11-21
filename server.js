ff0a8428387938742ce5048bde663d338e2a2fb3
Caleb Groff <cjgroff@email.arizona.edu>
2020-11-21 15:08:11 -0700

    Frist commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const { response } = require("express")
+const cors = require("cors")
+const app = express()
+const port = 3000
+const quizzesdata = require("./quizzesdata")
+const scores = []
+console.log("quizzes:",quizzesdata.quizzes)
+console.log("questions:",quizzesdata.questions)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/quizzes', (request, response) => {
+    //this is call if there is a GET request for /places
+    response.json(quizzesdata.quizzes)
+})
+
+app.get('/quiz/:id',(request, response) => {
+    let found = quizzesdata.questions["Quiz"+request.params.id]
+    response.json(found)
+})
+app.post('/score', (request, response) => {
+    console.log("score:", request.body)
+    scores.push(request.body)
+    console.log("Update scores:", scores)
+    response.json({message: 'The score saved'})
+
+}
+)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
+
+
